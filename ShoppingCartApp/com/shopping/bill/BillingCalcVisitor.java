9edfa33e08bd65ad94b264fa0384b137fdfe608f
thejavamentor <thejavamentor@gmail.com>
2020-04-27 12:47:34 +0530

    Generalized calculate method.

--- ShoppingCartApp/com/shopping/bill/BillingCalcVisitor.java
+++ ShoppingCartApp/com/shopping/bill/BillingCalcVisitor.java
@@ -7,26 +7,19 @@ import java.util.Map;
 public class BillingCalcVisitor implements  Visitor{
     @Override
     public int visit(RegularCustomer customer) {
-        int price = customer.getPrice();
-        int discount = 0;
-        Map<String, DiscountModel> map = customer.getDiscountMap();
-        for(Map.Entry<String,DiscountModel> entrySet : map.entrySet()){
-            String key = entrySet.getKey();
-            DiscountModel discountModel = entrySet.getValue();
-            if(price > discountModel.getMaxAmount()){
-                discount = discount + discountModel.getDefaultDiscPrice();
-            } else {
-                int diff = price - discountModel.getMinAmount();
-                discount = discount + (diff * discountModel.getDiscuntpercent())/100;
-                break;
-            }
-        }
-        customer.setDiscountedPrice(price-discount);
+        calculateDiscount(customer);
         return customer.getDiscountedPrice();
     }
 
+
     @Override
     public int visit(PremiumCustomer customer) {
+        calculateDiscount(customer);
+        return customer.getDiscountedPrice();
+    }
+
+
+    private void calculateDiscount(CustomerTypeIntf customer) {
         int price = customer.getPrice();
         int discount = 0;
         Map<String, DiscountModel> map = customer.getDiscountMap();
@@ -42,6 +35,5 @@ public class BillingCalcVisitor implements  Visitor{
             }
         }
         customer.setDiscountedPrice(price-discount);
-        return customer.getDiscountedPrice();
     }
 }
